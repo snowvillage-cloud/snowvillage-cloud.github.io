20199ca2941bbc8159224c79ed99712218fccfe5
j-y-matsubara <j.y.matsubara.git@gmail.com>
2026-03-23 05:26:11 +0900

    ct

---
+++ about/aboutData.js
@@ -0,0 +1,48 @@
+// about/aboutData.js
+
+// 🌟 運営メンバー（Mayors）のデータ
+const organizersData = [
+  {
+    name: "XXXXX",
+    role: "Organizer / Community Lead",
+    desc: "XXXXXX",
+    photo: "../images/organizers/photo_taro.jpg",
+    xUrl: "https://x.com/あなたのID",
+    linkedInUrl: "https://www.linkedin.com/in/あなたのID"
+  },
+  {
+    name: "Yuya Matsubara",
+    role: "Snowflake DataSuperheros 2026 & Snowflake Squad",
+    desc: "Principal Data Engineer at NTT DOCOMO, Inc. <br>Slackでなんかうるさいやつです!",
+    photo: "../images/organizers/yuya_matsubara.jpeg",
+    xUrl: "https://x.com/matsubara_tech",
+    linkedInUrl: "https://www.linkedin.com/in/yuya-matsubara-b86278323"
+  },
+  {
+    name: "Cloud Jiro",
+    role: "Public Relations",
+    desc: "XXXXXX",
+    photo: "../images/organizers/photo_jiro.jpg",
+    xUrl: "https://x.com/あなたのID",
+    linkedInUrl: "https://www.linkedin.com/in/あなたのID"
+  }
+];
+
+// 🌟 スポンサーのデータ
+const sponsorsData = [
+  {
+    name: "Yuya Matsubara",
+    desc: "Principal Data Engineer at NTT DOCOMO, Inc.",
+    logo: "../images/sponsors/yuya_matsubara.jpeg"
+  },
+  {
+    name: "XXXXXX",
+    desc: "XXXXXX",
+    logo: "../images/sponsors/sponsor_d.png"
+  },
+  {
+    name: "XXXXXX",
+    desc: "XXXXXX",
+    logo: "../images/sponsors/sponsor_e.png"
+  }
+];
