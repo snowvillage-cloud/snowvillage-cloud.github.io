5aa4381a1b09a5c2ba5f62ffe39f9c2ec7a7104d
j-y-matsubara <j.y.matsubara.git@gmail.com>
2026-04-22 03:23:17 +0900

    Fix

--- newsData.js
+++ newsData.js
@@ -1,54 +1,17 @@
 // --- 🌟 お知らせデータ管理 (newsData.js) ---
 const newsData = [
     {
-        date: "2026.XX.XX",
+        date: "2026.04.22",
         text: "SnowVillage運営体制についてのお知らせ",
         link: "events/", 
         isExternal: false
     },
     {
-        date: "2026.XX.XX",
+        date: "2026.04.22",
         text: "SnowVillage公式サイトをリニューアルオープンしました！",
         link: "https://snowvillage.cloud/", // events/とかでも良い
         isExternal: true //　内部の場合は false
-    },
-    {
-        date: "2026.XX.XX",
-        text: "sample",
-        link: "events/", 
-        isExternal: false
-    },
-    {
-        date: "2026.XX.XX",
-        text: "sample",
-        link: "events/", 
-        isExternal: false
-    },
-    {
-        date: "2026.XX.XX",
-        text: "sample",
-        link: "events/", 
-        isExternal: false
-    },
-    {
-        date: "2026.XX.XX",
-        text: "sample",
-        link: "events/", 
-        isExternal: false
-    },
-    {
-        date: "2026.XX.XX",
-        text: "sample",
-        link: "events/", 
-        isExternal: false
-    },
-    {
-        date: "2026.XX.XX",
-        text: "sample",
-        link: "events/", 
-        isExternal: false
     }
-
     /* 新しいニュースを追加する場合は、ここにオブジェクトを足していくだけです
     {
         date: "2026.04.01",
